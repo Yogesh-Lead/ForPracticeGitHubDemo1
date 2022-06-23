51e46362ca3d2ee93eff1dab303f14b2fa5ac161
Yogesh-Lead <yogesh.moremcoe@gmail.com>
2022-06-23 13:42:25 +0530

    First commit to show the Git Demo to my friends

---
+++ src/test/java/GitDemoPackage3/TestGit1.java
@@ -0,0 +1,15 @@
+package GitDemoPackage3;
+
+public class TestGit1 {
+	public static void main(String[] args) {
+
+		String hungrey = "Yes";
+		if (hungrey == "Yes") {
+			System.out.println("Eat something");
+		} else {
+			System.out.println("Do some work");
+		}
+
+	}
+
+}
